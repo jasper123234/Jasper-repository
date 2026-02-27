8e4b694e0edb98c20bd5163acca7bf4331f67d4e
jasper123234 <jasper.cronin@gmail.com>
2026-02-27 11:12:34 -0800

    d

--- Jasper React/src/App.jsx
+++ Jasper React/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Practice from './Practice'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <> 
       <div>
-        import Practice from './Practice'
+        
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>

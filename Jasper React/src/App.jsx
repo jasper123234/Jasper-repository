59ef695b624df0a9f81f70ce7320bc8a71dac5f8
jasper123234 <jasper.cronin@gmail.com>
2026-02-27 11:07:53 -0800

    f

--- Jasper React/src/App.jsx
+++ Jasper React/src/App.jsx
@@ -7,8 +7,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <> import Practice from './Practice'
+    <> 
       <div>
+        import Practice from './Practice'
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
